365b6b3d482dce022507e8d47115b31c51b85310
jwc20 <choij2015@gmail.com>
2023-03-23 09:30:41 -0700

    Format

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 set -g fish_greeting
-
 set -gx TERM xterm-256color
 
 # theme
@@ -9,26 +8,24 @@ set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
-
+# alias and commands
 alias vi='nvim'
 alias lg='lazygit'
 alias lzd='lazydocker'
 alias ls='ls'
 alias cl='clear'
 alias mp='multipass'
-
+fzf_configure_bindings --directory=\cf --variables=\cv --git_log=\cl --git_status=\cs --history=\ch --processes=\cp
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 function nvm
-   bass source $HOME/.nvm/nvm.sh --no-use ';' nvm $argv
+    bass source $HOME/.nvm/nvm.sh --no-use ';' nvm $argv
 end
-
 
 starship init fish | source
 
 # fzf.fish
-fzf_configure_bindings --directory=\cf --variables=\cv --git_log=\cl --git_status=\cs --history=\ch --processes=\cp
 set fzf_fd_opts --hidden --exclude=.git
