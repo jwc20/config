e2ff0c1d2e012bc235935e7cc989140cb1e123d5
jwc20 <choi.jae100@gmail.com>
2025-06-29 17:18:48 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 set -g fish_greeting
 set -gx TERM xterm-256color
 
+set --export ANDROID_HOME "$HOME/Library/Android/sdk"
+set --export PATH "$PATH:$ANDROID_HOME/emulator"
+set --export PATH "$PATH:$ANDROID_HOME/platform-tools"
+
 # theme
 set -g theme_color_scheme terminal-dark
 set -g fish_prompt_pwd_dir_length 1
@@ -8,16 +12,34 @@ set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+# set -Ux PYENV_ROOT $HOME/.pyenv
+# set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 
+# Add pyenv to PATH and initialize it
+# export PATH="$HOME/.pyenv/bin:$PATH"
+# eval "$(pyenv init --path)"
+# eval "$(pyenv init -)"
+
+# set REACT_EDITOR 'code --wait'
+export REACT_EDITOR='code'
 
 # alias and commands
 alias vi='nvim'
+alias python='python3'
+alias pip='pip3'
 alias lg='lazygit'
 alias lzd='lazydocker'
 alias cl='clear'
 alias mp='multipass'
+alias dcu='docker-compose up -d --build'
+alias dcd='docker-compose down'
+
+
+alias claude='sudo SHELL=/bin/bash claude'
+
+
+# alias python='python3'
+
 fzf_configure_bindings --directory=\cf --variables=\cv --git_log=\cl --git_status=\cs --history=\ch --processes=\cp
 
 
@@ -45,13 +67,28 @@ function git_status_current_dir
     end
 end
 
-alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
+# alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
 
 
 # set -Ux PYENV_ROOT $HOME/.pyenv
 # fish_add_path $PYENV_ROOT/bin
 
-pyenv init - | source
-status --is-interactive; and source (pyenv virtualenv-init -|psub) 
+# pyenv init - fish | source
+
+# pyenv init - | source
+# status --is-interactive; and source (pyenv virtualenv-init -|psub) 
+
+eval (direnv hook fish)
 
 starship init fish | source
+
+set -x PATH path_to_abd $PATH
+
+# Created by `pipx` on 2024-06-15 11:24:41
+set PATH $PATH /Users/cjw/.local/bin
+
+# Added by `rbenv init` on Sat Aug 17 13:12:00 KST 2024
+status --is-interactive; and rbenv init - --no-rehash fish | source
+
+
+set -gx PATH $PATH /Users/cjw/.nvm/versions/node/v22.14.0/bin
